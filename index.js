f6777abfa21352894d4a8939e540eae8fa7fb7e8
lajosdeme <lajosd@protonmail.ch>
2023-11-18 15:04:29 +0300

    add filecoin upload and ethers signature verification

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const cors = require('cors');
 const {
     exec
 } = require('child_process');
+const lighthouse = require("@lighthouse-web3/sdk");
+require('dotenv').config()
+const ethers = require("ethers");
+const { hashMessage } = require("@ethersproject/hash");
+const mediscanAbi = require("./abis/mediscan.json")
 
 const app = express();
-const port = 3000;
+const port = 8080;
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -24,7 +29,7 @@ const upload = multer({
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post("/api/new", upload.single('image'), (req, res) => {
+app.post("/api/new", upload.single('image'), async (req, res) => {
     const command = 'python3 scripts/new.py';
     try {
         const image = req.file;
@@ -32,21 +37,40 @@ app.post("/api/new", upload.single('image'), (req, res) => {
         const imgName = "image.jpeg";
         fs.writeFileSync(imgName, image.buffer);
 
-        exec(command, (error, stdout, stderr) => {
+        exec(command, async (error, stdout, stderr) => {
             if (error) {
                 console.error(`Error executing the script: ${error}`);
                 return;
             }
             // Print the output of the Python script
             console.log(`Python script output: ${stdout}`);
+            let id = stdout.replace(/\n/g, '');
+
+            let uploadResult = await uploadFile(id, jsonData);
+
+            fs.readFile('face_hash_health_data.json', (err,data) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Internal Server Error');
+                }
+                let existingData = JSON.parse(data);
+                existingData[id] = uploadResult.data.Hash;
+                fs.writeFile('face_hash_health_data.json', JSON.stringify(existingData, null, 2), 'utf8', (err) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).send('Internal Server Error');
+                    }
+                })
+            })
+
             fs.readFile('data.json', 'utf8', (err, data) => {
                 if (err) {
                     console.error(err);
                     return res.status(500).send('Internal Server Error');
                 }
                 let existingData = JSON.parse(data);
-                let id = stdout.replace(/\n/g, '');
                 existingData[id] = jsonData;
+
                 fs.writeFile('data.json', JSON.stringify(existingData, null, 2), 'utf8', (err) => {
                     if (err) {
                         console.error(err);
@@ -90,22 +114,17 @@ app.post('/api/find', upload.single('image'), (req, res) => {
             console.log(`Python script output find: ${stdout}`);
             let id = stdout.replace(/\n/g, '');
 
-            fs.readFile('data.json', 'utf8', (err, data) => {
+            fs.readFile("face_hash_health_data.json", 'utf8', async (err,data) => {
                 if (err) {
                     console.error(err);
                     return res.status(500).send('Internal Server Error');
                 }
                 let existingData = JSON.parse(data);
-                let jsonData = existingData[id];
+                let cid = existingData[id];
+                let health_record = await downloadFile(cid);
+                console.log("download result: ", health_record);
                 res.json({
-                    jsonData
-                });
-
-
-                fs.unlink(imgName, (err) => {
-                    if (err) {
-                        console.log("error deleting file: ", err)
-                    }
+                    health_record
                 })
             })
         });
@@ -120,3 +139,56 @@ app.post('/api/find', upload.single('image'), (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
+
+async function uploadFile(faceHash, healthData) {
+    const apiKey = process.env.LIGHTHOUSE_KEY;
+    const response = await lighthouse.uploadText(healthData, apiKey, faceHash);
+    console.log("Upload response: ", response)
+    return response
+}
+
+async function downloadFile(cid) {
+    try {
+        const response = await fetch(`https://gateway.lighthouse.storage/ipfs/${cid}`);
+
+        // Check if the request was successful (status code in the range of 200-299)
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        // Parse the JSON response
+        const data = await response.json();
+
+        // Print the JSON data
+        console.log(data);
+        return data;
+    } catch (error) {
+        // Handle errors
+        console.error('Error fetching data:', error.message);
+    }
+}
+
+async function verifyUserAdmin() {
+    const message = "mediscan"
+
+    const provider = new ethers.providers.AlchemyProvider('maticmum', process.env.ALCHEMY_KEY)
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    
+    const signedMessage = await wallet.signMessage(message);
+    const signerAddress = ethers.utils.recoverAddress(hashMessage(message), signedMessage)
+    console.log("Signer address: ", signerAddress)
+    
+    const mediscan = new ethers.Contract(process.env.MEDISCAN_ADDRESS, mediscanAbi);
+
+    const res = await mediscan.getAccessLevelForAddress(signerAddress);
+
+    if (res == 0) {
+        console.log("Full access");
+    } else if (res == 1) {
+        console.log("Medium access")
+    } else if (res == 2) {
+        console.log("Low access")
+    } else {
+        console.log("No access")
+    }
+}
